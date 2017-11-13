c317a6da27eca5c623cbae1c7399c2302deeed06
PAYEN Tiphaine <payen8u@tncy-1-18-49.telecomnancy.site.univ-lorraine.fr>
2017-11-13 16:49:45 +0100

    modification main

--- src/main.java
+++ src/main.java
@@ -1,4 +1,17 @@
-//
-public class main {
+import java.util.ArrayList;
 
+import data.LectureData;
+
+public class main {
+	
+	public static void main(String[] args){
+	LectureData ld = new LectureData();
+	ld.Parsing();
+	//System.out.println(ld.dataTrain.get(1));
+	ArrayList<String> champs = new ArrayList<String>();
+	champs.add("Entry");
+	champs.add("Cross-reference (ProDom)");
+	ld.RowSelection(champs);
+	System.out.println(ld.dataTrainFinal.get(0).size());
+	}
 }
