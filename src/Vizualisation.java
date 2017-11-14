f754e747c665793fded4975aedfae12c07d4fb7b
Martins Melvin <melvin-martins@hotmail.fr>
2017-11-14 09:54:09 +0100

    Ebauche de la classe de visu du projet

---
+++ src/Vizualisation.java
@@ -0,0 +1,26 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Rectangle;
+import java.awt.geom.Rectangle2D;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.JApplet;
+
+import org.jgraph.JGraph;
+import org.jgraph.graph.DefaultGraphCell;
+import org.jgraph.graph.GraphConstants;
+
+import org.jgrapht.ListenableGraph;
+import org.jgrapht.ext.JGraphModelAdapter;
+import org.jgrapht.graph.ListenableDirectedGraph;
+import org.jgrapht.graph.DefaultEdge;
+
+/**
+ * Class used to vizualise the graphs created. The global idea is 
+ * an input file text parsed to create the corresponding graph which has to 
+ * be clear for the user.
+ */
+public class Vizualisation extends JApplet {
+	
+}
